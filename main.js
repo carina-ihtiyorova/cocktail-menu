82d1e4dc67ab0b7de58ff38eae0557f9667d3be2
Aiperi Karabalaeva <karabalaevaiperi@gmail.com>
2021-10-09 11:09:45 -0700

    fixed searched cocktails

--- main.js
+++ main.js
@@ -29,33 +29,42 @@ searchInput.addEventListener("input", searchStrDrink)
 
 function searchStrDrink() {
    let div = document.querySelectorAll(".cocktail")
-   div.forEach(el => el.innerHTML = "")
+   div.forEach(el => {
+      // el.removeEventListener("click",cocktailIdSearch)
+      el.innerHTML = ""
+
+   })
    let filteredCoctailName = cocktails.filter(el => {
       return el.strDrink.toLowerCase().includes(searchInput.value.toLowerCase());
    })
    filteredCoctailName.forEach((el, index) => {
       let filteredCoctails = `
       <div class="cocktail">
-      <img src="${el.strDrinkThumb}" />
-      <p class="cocktail-name">${el.strDrink}</p> 
+      <img src="${el.strDrinkThumb}" id="${el.idDrink}" />
+      <p class="cocktail-name" id="${el.idDrink}">${el.strDrink}</p> 
       </div>`;
       div[index].innerHTML = filteredCoctails
+      div[index].addEventListener("click", cocktailIdSearch)
    })
+}
+function cocktailIdSearch(event) {
+   let id = event.target.id
+   localStorage.setItem("currentCoctailId", id)
 }
 ////////////////////////////////////Link to New HTML /////////////////////////
 ///////we have a problem:when searched for drinks our click function to another html isnt working because we picked div.cocktail array but inside  searchstrdrink we are looping thru .cocktails-block so i changed line 31 to our line 47 but now when clicked on the searched elements its not showing anything why?/////////////
 
-const block = document.querySelectorAll(".cocktail");
-block.forEach(el => {
-   el.addEventListener("click", function () {
-      location.href = "drink.html";
-   })
-})
-block.forEach(el => {
-   el.addEventListener("click", cocktailId)
+// const block = document.querySelectorAll(".cocktail");
+// block.forEach(el => {
+//    el.addEventListener("click", function () {
+//       location.href = "drink.html";
+//    })
+// })
+// block.forEach(el => {
+//    el.addEventListener("click", cocktailId)
 
-   function cocktailId(event) {
-      let id = event.target.id
-      localStorage.setItem("currentCoctailId", id)
-   }
-})
+//    function cocktailId(event) {
+//       let id = event.target.id
+//       localStorage.setItem("currentCoctailId", id)
+//    }
+// })
