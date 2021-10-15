72d8f0f5282a57d372f8340918d868a2c53a2b08
carina <benazirihtiyorova22@gmail.com>
2021-10-15 16:43:09 -0700

    added comments

--- drink.js
+++ drink.js
@@ -1,29 +1,33 @@
 import {
   cocktails
 } from "./data.js";
-const drinkWrapper = document.querySelector(".drink-wrapper")
+///////////////bringing one drinks object when clicked///////////////////////
 let find = cocktails.find(el => {
+  console.log(el, "drinks")
   return el.idDrink === localStorage.getItem("currentCoctailId")
 })
-
+////////////////////changing drink htmls innerhtml ////////////////////////////////////////////////////
 let drinkRecieps = `
-          <div class = "img">
-          <img src = "${find.strDrinkThumb}"/>
-          </div>
-           <div class = "drink-info" >
-          <h3 class = "cocktail-name-drink" > Drink Name: ${ find.strDrink} </h3>
-          <h3 class = "cocktail-category" > Type Of Drink: ${ find.strCategory} </h3>
-          <h3 class = "cocktail-instruction" > Instructions: ${ find.strInstructions} </h3>
-          <h3 class = "h3-last">ingredients: </h3>
-          <button class="showBtn">SHOW ALL COCKTAILS </button>
-          </div>`
+<div class = "img">
+<img src = "${find.strDrinkThumb}"/>
+</div>
+<div class = "drink-info" >
+<h3 class = "cocktail-name-drink" > Drink Name: ${ find.strDrink} </h3>
+<h3 class = "cocktail-category" > Type Of Drink: ${ find.strCategory} </h3>
+<h3 class = "cocktail-instruction" > Instructions: ${ find.strInstructions} </h3>
+<h3 class = "h3-last">ingredients: </h3>
+<button class="showBtn">SHOW ALL COCKTAILS </button>
+</div>`
+const drinkWrapper = document.querySelector(".drink-wrapper")
 let div = document.createElement('div');
 div.setAttribute("class", "cocktails-blocks-drink")
 div.innerHTML = drinkRecieps;
 drinkWrapper.appendChild(div);
+/////////////////////////creating ingredients dynamically/////////////////////////////
 let drinkInfo = document.querySelector(".h3-last")
 let ul = document.createElement('ul');
 for (let el in find) {
+  console.log(el)
   if (el.includes("strIngredient")) {
     let li = document.createElement("li")
     li.innerHTML = find[el];
@@ -31,7 +35,7 @@ for (let el in find) {
   }
 }
 drinkInfo.appendChild(ul)
-///////////////////////////BUTTON/////////////////////////////////
+///////////////////////////BUTTON TO GO BACK TO MAIN PAGE/////////////////////////////////
 let button = document.querySelector(".showBtn")
 button.addEventListener("click", display)
 
